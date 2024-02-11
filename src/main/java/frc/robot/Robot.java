5dd62abc6a3a43867625fbf82c8ee188213c54af
ozziexyz <ozzieh@skiff.com>
2024-02-11 15:37:16 -0500

    correct following and factory defaults

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,15 +7,10 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
   private DifferentialDrive m_robotDrive;
   private CommandXboxController m_controller = new CommandXboxController(0);
@@ -27,15 +22,14 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    m_leftBackMotor.restoreFactoryDefaults();
+    m_rightBackMotor.restoreFactoryDefaults();
+    m_leftFrontMotor.restoreFactoryDefaults();
+    m_rightFrontMotor.restoreFactoryDefaults();
+    
     m_leftBackMotor.follow(m_leftFrontMotor);
-    m_rightFrontMotor.follow(m_rightFrontMotor);
-
-    SendableRegistry.addChild(m_robotDrive, m_leftFrontMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightFrontMotor);
-
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightBackMotor.follow(m_rightFrontMotor);
+    
     m_rightFrontMotor.setInverted(true);
     m_rightBackMotor.setInverted(true);
 
